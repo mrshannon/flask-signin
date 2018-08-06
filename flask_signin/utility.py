120f826f76d4ee47260e726fc44ad9ab10fa429b
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-08-06 13:30:02 -0600

    Add unit testing.

---
+++ flask_signin/utility.py
@@ -0,0 +1,9 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+
+def true_function():
+    return True
+
+def false_function():
+    return False
---
+++ tests/test_utility.py
@@ -0,0 +1,9 @@
+import pytest
+from flask_signin.utility import true_function, false_function
+
+
+def test_true_function():
+    assert true_function()
+
+def test_false_function():
+    assert not false_function()
